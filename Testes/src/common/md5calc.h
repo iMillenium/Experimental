74eb2da28e763ca0cff3128cfd679a7afa5302f5
Diego da Silva Pinheiro <diegopinheiro.sjc@hotmail.com>
2014-05-26 18:12:59 -0300

    Bye Testes
    Signed-off-by: Diego da Silva Pinheiro <diegopinheiro.sjc@hotmail.com>

--- Testes/src/common/md5calc.h
+++
@@ -1,8 +0,0 @@
-#ifndef _COMMON_MD5CALC_H_
-#define _COMMON_MD5CALC_H_
-
-void MD5_String(const char * string, char * output);
-void MD5_Binary(const char * string, unsigned char * output);
-void MD5_Salt(unsigned int len, char * output);
-
-#endif /* _COMMON_MD5CALC_H_ */
